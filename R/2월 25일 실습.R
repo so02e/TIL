b9b5a68c8ca315c5a9482c6ce0e31dc807b82965
Soyoung Jang <76713906+so02e@users.noreply.github.com>
2021-02-25 17:00:30 +0900

    Update 2월 25일 실습.R
    NA를 우선 비교하자.

--- R/2월 25일 실습.R
+++ R/2월 25일 실습.R
@@ -1,10 +1,6 @@
 # 문제 1
-exam1 <- function() return (paste0(LETTERS,letters))
+exam1 <- function() return (paste0(LETTERS,letters)) # 두 벡터를 각각의 원소단위로 묶어 출력할 수 있다.
 exam1()
-
-
-
-
 
 
 # 문제 2
@@ -15,11 +11,15 @@ exam2 <- function(number){
   }
   return(answer)
 }
-
 exam2(4)
 exam2(100)
 
 
+exam2_2 <- function(num){
+  return(sum(1:num)) # (1:num까지 구성된 벡터)
+}
+exam2_2(4)
+exam2_2(100)
 
 
 
@@ -75,6 +75,8 @@ exam4(0,"%/%",4)
 exam4(0,"%%",4)
 exam4(2,"%/%",0)
 exam4(2,"%%",0)
+exam4(2,"/",5)
+
 
 
 
@@ -86,7 +88,6 @@ exam5 <- function(num1, str1="#"){
     for (i in 1:num1){
       cat(str1)
     }
-  else
   return()
 }
 
@@ -95,18 +96,33 @@ exam5(3,)
 exam5(-3,) #NULL 도 출력 되지 않도록?
 
 
+exam5_2 <- function(a,b="#"){
+  if (a <= 0){
+    return()
+  }
+  else {
+    for (i in 1:a){
+      cat(b)
+    }
+  }
+}
+
+exam5_2(4,"$")
+exam5_2(3,)
+exam5_2(-3,) 
 
 
 
 
 # 문제 6
 exam6 <- function(score){
-  end <- length(score)
+  end <- length(score) 
   for (i in 1:end) {
     if(is.na(score[i])) # 제일 위로 올려야 하는 이유는 ?
-      print("NA는 처리 불가")
+      print("NA는 처리 불가") #NA 우선 처리 !!!! 
+    
     else if (score[i] >= 85)
-      print(paste(score[i],"점은 상등급입니다"))
+      print(paste(score[i],"점은 상등급입니다")) # cat을 쓰게 되면, paste를 안 써도 된다. 
     else if(score[i] >= 70)
       print(paste(score[i],"점은 중등급입니다"))
     else if(score[i] >= 0)
@@ -116,3 +132,49 @@ exam6 <- function(score){
 }
 
 exam6(c(80,50,70,66,NA,35))
+
+
+
+
+exam6_1 <- function(score){
+  end <- length(score) 
+  for (i in 1:end) {
+    if (score[i] >= 85)
+      print(paste(score[i],"점은 상등급입니다")) # cat을 쓰게 되면, paste를 안 써도 된다. 
+    else if(score[i] >= 70)
+      print(paste(score[i],"점은 중등급입니다"))
+    else if(score[i] >= 0)
+      print(paste(score[i],"점은 하등급입니다"))
+    else if(is.na(score[i])) #결과는 무조건 NA이다. NA는 NOT AVAILABLE 결측치(무한대) , TRUE FALSE 자리에 NA가 와버렸기 때문에 오류. 
+      print("NA는 처리 불가")  
+  }
+  return()
+}
+
+exam6_1(c(80,50,70,66,NA,35))
+
+
+
+
+
+# 아직 안 배운 부분 (...)
+exam6_2 <- function(...){
+  data <- c(...)
+  for(score in data){
+    if(!is.na(score)){
+      if(score >= 85){
+        lev = '상'}
+      else if(score >= 70){
+        lev = "중"}
+      else {
+        lev = "하"}
+      print(paste(score, "점은", lev, '등급입니다.'))
+    }
+    else {
+      print("NA는 처리불가")
+    }
+  }
+  return()
+}
+  
+exam6_2(80,50,70,66,NA,35)
