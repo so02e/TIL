cb1e0d10e620dafd158b9b4b010c4e9e3685b3df
Soyoung Jang <76713906+so02e@users.noreply.github.com>
2021-03-03 15:55:49 +0900

    API 관련 실습
    행단위로 불러온 파일을 대괄호에 행을 넣어서 gsub함수를 이용할 수 있다.

--- R/3월 2일 실습.R
+++ R/3월 2일 실습.R
@@ -1,7 +1,12 @@
 # 추출된 숫자에 해당하는 알파벳 대문자를 원소값으로 구성된 벡터를 생성하여 리턴하는 코드
-
 v <- sample(1:26,10)
 v
-
 # FUN의 반환 값이 1보다 크다면 행렬을 반환하므로, 리턴값 자체가 벡터가 되도록
-sapply(v,function(d) result <- c(LETTERS[d]))
+sapply(v,function(d) result <- LETTERS[d]) # 굳이 c()함수에 넣을 필요는 없다.
+
+
+v2 <- sample(1:26,10)
+vran <- function(v){return(LETTERS[v])}
+sapply(v2,vran)
+
+# 여러 번 사용할 것이면 함수를 만들어 사용하고, 그렇지 않다면 apply계열의 fun에 함수를 만들어도 된다.
--- R/3월 2일 실습2.R
+++ R/3월 2일 실습2.R
@@ -1,4 +1,5 @@
 # 제시된 memo.txt 파일을 행 단위로 읽어서 벡터를 리턴한다.
+# 대괄호로 묶으면, '또는'의 의미를 갖는다.
 
 memo <- readLines("data/memo.txt",encoding="UTF-8")
 
@@ -16,11 +17,16 @@ memo <- gsub("정착", "(정착)", memo)
 memo <- gsub("OK", "ok?", memo) 
 memo  <- gsub("YOU", "you", memo) 
 
-# 대문자로 시작하는 단어 지우기
-memo <- gsub("^[A-Z]","",memo)
+# 대문자로 시작하는 단어 지우기 # memo[4] <- gsub("[[a-z][A-Z]]","",memo[4])
+memo[4] <- gsub("[A-Z]","",memo[4])
+memo[4] <- gsub("[a-z]","",memo[4])
+
 
 memo <- gsub("e", "E", memo) 
 
+# 행 단위로 정규표현식을 줄 수 있다.
+memo[6] <- gsub("[[:space:]]", "", memo[6])
+
 memo
 
-write(memo,file="memo_new.txt")
+write(memo,file="data/memo_new.txt") # 폴더명/파일명
