5ce6dfd05a0a45c1d42f13d5ee51d58779f3f229
Soyoung Jang <76713906+so02e@users.noreply.github.com>
2021-03-03 16:21:08 +0900

    CSS 선택자와 rvest
    ol 태그의 자식 태그 : ol>*
    table 태그의 자손 : table *
    name클래스 속성의 tr : tr.name
    target id의 td : td#target

--- R/3월 3일 실습.R
+++ R/3월 3일 실습.R
@@ -4,7 +4,7 @@ url <- "http://unico2013.dothome.co.kr/crawling/exercise_bs.html"
 text <- read_html(url) # url 문자열
 
 # (1) <h1> 태그의 컨텐츠 
-h1 <- html_text(html_nodes(text, "h1"))
+h1 <- html_text(html_nodes(text, "h1")) # 하나가져오는 것이 확실하므로 html_node를 써도 된다.
 h1
 
 # (2) <a> 태그의 href 속성값  
@@ -21,20 +21,23 @@ node1 <- html_text(html_nodes(text, "h2:nth-of-type(1)"))
 node1
 
 # (5) <ul> 태그의 자식 태그들 중 style 속성의 값이 green으로 끝나는 태그의 컨텐츠 (다시)
-ul <- html_text(html_nodes(text,"ul>li[style] color:green"))
+# [] : 속성 선택자. $는 green 으로 끝나는 ^ green 으로 시작하는 .. (캐럿문자열)
+ul <- html_text(html_nodes(text,"ul>li[style$='green']"))
 ul
 
 # (6) 두 번째 <h2> 태그의 컨텐츠
 node2 <- html_text(html_nodes(text, "h2:nth-of-type(2)"))
 node2
 
-# (7) <ol> 태그의 모든 자식 태그들의 컨텐츠 
-# 자식과 자손을 치지 않고 바로 부르는 방법? 
-ol <- html_text(html_nodes(text,"ol>li"))
+# (7) <ol> 태그의 모든 자식 태그들의 컨텐츠 (ol>*)
+#ol <- html_text(html_nodes(text,"ol>li"))
+ol <- html_text(html_nodes(text,"ol>*"))
 ol
 
-# (8) <table> 태그의 모든 자손 태그들의 컨텐츠
-table <- html_text(html_nodes(text,"table > tr,th,td"))
+# (8) <table> 태그의 모든 자손 태그들의 컨텐츠 ("table *)
+#table <- html_text(html_nodes(text,"table > tr,th,td"))
+table <- html_text(html_nodes(text,"table *"))
+
 table
 
 # (9) name이라는 클래스 속성을 갖는 <tr> 태그의 컨텐츠
@@ -42,5 +45,5 @@ tr <- html_text(html_nodes(text, "tr.name"))
 tr
 
 # (10) target이라는 아이디 속성을 갖는 <td> 태그의 컨텐츠
-td <- html_text(html_nodes(text,"#target"))
+td <- html_text(html_nodes(text,"td#target"))
 td
